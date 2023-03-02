812b273816228cbb194852409334e50d57c3e23f
ConnerKT <connerthompson121@gmail.com>
2023-03-01 21:29:55 -0600

    Added DOM
    DOM on list for the cookies per hour for each hour

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 // Grabbing random number of customers from our min and max
-
 function getRandom(max, min) {
     let random = Math.random() * (max - min) + min;
     
@@ -18,37 +17,107 @@ let seattle = {
     cookiesperhour:[],
     getcookies: function() {
         for (i = 0; i < this.hoursofoperation.length; i++ ) {
+            //console.log("this.avg", this.avg)
+            //console.log("this.customersperhour()", this.customersperhour())
+            let cookies = Math.ceil(this.avg * this.customersperhour())
+            this.cookiesperhour.push(cookies)
+        }
+        return this.cookiesperhour 
+    }
+
+}
+// calls each function so the array of cookies is put in array (if not its undefined)
+seattle.getcookies();
+tokyo.getcookies();
+dubai.getcookies();
+paris.getcookies();
+lima.getcookie();
+// makes List for each city
+let hoursArray = document.querySelector('.output')
+for (let i = 0; i < seattle.hoursofoperation.length; i++){
+    let variable = document.createElement("li");
+    variable.innerHTML = `${seattle.hoursofoperation[i]}: ${seattle.cookiesperhour[i]} cookies | ${tokyo.hoursofoperation[i]}: ${toyko.cookiesperhour[i]} cookies | ${dubai.hoursofoperation[i]}: ${dubai.cookiesperhour[i]} cookies | ${paris.hoursofoperation[i]}: ${paris.cookiesperhour[i]} cookies | ${lima.hoursofoperation[i]}: ${lima.cookiesperhour[i]} cookies`;
+    hoursArray.append(variable);
+}
+
+let tokyo = {
+    min:3,
+    max:24,
+    avg:1.2,
+    hoursofoperation:["6am","7am","8am","9am","10am","11am","12am","1pm","2pm","3pm","4pm","5pm","6pm","7pm"],
+    customersperhour: function() {
+        return getRandom(this.max, this.min)
+    },
+    cookiesperhour:[],
+    getcookies: function() {
+        for (i = 0; i < this.hoursofoperation.length; i++ ) {
             console.log("this.avg", this.avg)
             console.log("this.customersperhour()", this.customersperhour())
             let cookies = Math.ceil(this.avg * this.customersperhour())
             this.cookiesperhour.push(cookies)
         }
-        return this.cookiesperhour
+        return this.cookiesperhour 
     }
-    
+
 }
-let tokyo = {
-    min:3,
-    max:24,
-    avg:1.2,
-    //cookies: customers * tokyo.avg,
-}
+
 let dubai = {
     min:11,
     max:38,
     avg:3.7,
-    //cookies: customers * dubai.avg,
+    hoursofoperation:["6am","7am","8am","9am","10am","11am","12am","1pm","2pm","3pm","4pm","5pm","6pm","7pm"],
+    customersperhour: function() {
+        return getRandom(this.max, this.min)
+    },
+    cookiesperhour:[],
+    getcookies: function() {
+        for (i = 0; i < this.hoursofoperation.length; i++ ) {
+            console.log("this.avg", this.avg)
+            console.log("this.customersperhour()", this.customersperhour())
+            let cookies = Math.ceil(this.avg * this.customersperhour())
+            this.cookiesperhour.push(cookies)
+        }
+        return this.cookiesperhour 
+    }
+
 }
 let paris = {
     min:20,
     max:38,
     avg:2.3,
-    //cookies: customers * paris.avg,
+    hoursofoperation:["6am","7am","8am","9am","10am","11am","12am","1pm","2pm","3pm","4pm","5pm","6pm","7pm"],
+    customersperhour: function() {
+        return getRandom(this.max, this.min)
+    },
+    cookiesperhour:[],
+    getcookies: function() {
+        for (i = 0; i < this.hoursofoperation.length; i++ ) {
+            console.log("this.avg", this.avg)
+            console.log("this.customersperhour()", this.customersperhour())
+            let cookies = Math.ceil(this.avg * this.customersperhour())
+            this.cookiesperhour.push(cookies)
+        }
+        return this.cookiesperhour 
+    }
+
 }
 let lima = {
     min:2,
     max:16,
     avg:4.6,
-    //cookies: customers * lima.avg,
-}
+    hoursofoperation:["6am","7am","8am","9am","10am","11am","12am","1pm","2pm","3pm","4pm","5pm","6pm","7pm"],
+    customersperhour: function() {
+        return getRandom(this.max, this.min)
+    },
+    cookiesperhour:[],
+    getcookies: function() {
+        for (i = 0; i < this.hoursofoperation.length; i++ ) {
+            console.log("this.avg", this.avg)
+            console.log("this.customersperhour()", this.customersperhour())
+            let cookies = Math.ceil(this.avg * this.customersperhour())
+            this.cookiesperhour.push(cookies)
+        }
+        return this.cookiesperhour 
+    }
 
+}
